bbc2d43eff3e13f5d3a73d8d263ecc04141d9956
asadbekumarov <asadbekumarov922@gmail.com>
2024-06-23 01:37:21 +0500

    2-dasr

---
+++ js/main.js
@@ -0,0 +1,48 @@
+/*
+1-Matter
+
+var number =10;
+var number2 =5;
+var sum = a + b 
+console.log(typeof sum, sum);
+*/
+
+// 2-Matter
+
+/* 
+var number =25;
+var number2 =15;
+var result = a - b
+console.log(result); 
+*/
+
+// 3-Matter
+
+/*
+var number = 10;
+var number2 = 5;
+var number3 = a - b
+console.log(c);
+var number4 = 7;
+var result = d * c
+console.log(result);
+*/
+
+
+// 4-Matter
+
+/*
+var number = 10
+var number2 = 3 
+var sum = 10 % 3
+console.log(typeof sum ,sum)
+*/
+  
+// 5-Matter
+/*
+var number =2;
+var number2 =10;
+var sum = 2 ** 10
+console.log(typeof sum, sum)
+*/
+
